60efe9fb66c14876638a5fbdbffb18c8d06dbef0
Leah Borns <leah.borns@gmail.com>
2018-05-11 12:16:52 -0700

    added hamburger menu

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ $(".jumper").on("click", function( e ) {
     }, 600);
 
 });
+
+
+var mobileBtn = document.querySelector('.mobile-btn');
+var menu = document.querySelector('ul#nav-links');
+
+var closeBtn = document.querySelector('.close');
+var overlay = document.querySelector('.overlay');
+
+mobileBtn.addEventListener('click', function() {
+    menu.className += ' open';
+    overlay.className += ' open';
+})
+
+closeBtn.addEventListener('click', function() {
+    menu.className += 'menu';
+    overlay.className = 'overlay';
+})
+
+window.addEventListener('click', function(event) {
+    if(event.target === overlay) {
+        menu.className += 'menu';
+        overlay.className = 'overlay';
+    }
+})
