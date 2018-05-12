4a8e9b071988f7dc95aede33098704b6990f1c8f
Leah Borns <leah.borns@gmail.com>
2018-05-11 17:01:01 -0700

    notes for later

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//animates the scroll to go smoothly to each section
+
 $(".jumper").on("click", function( e ) {
 
     e.preventDefault();
@@ -15,15 +17,23 @@ var menu = document.querySelector('ul#nav-links');
 var closeBtn = document.querySelector('.close');
 var overlay = document.querySelector('.overlay');
 
+
+//opens the menu when click on hamburger icon
+
 mobileBtn.addEventListener('click', function() {
     menu.className += ' open';
     overlay.className += ' open';
 })
 
+//closes menu when click on 'x'
+
 closeBtn.addEventListener('click', function() {
     menu.className += 'menu';
     overlay.className = 'overlay';
 })
+
+
+//closes menu when clicking anywhere in the window
 
 window.addEventListener('click', function(event) {
     if(event.target === overlay) {
